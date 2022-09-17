c023f35ff6d23d09e577d83d7aeb306979334abf
Cozmaaa <luncancosmin@gmail.com>
2022-09-17 04:26:56 +0300

    Added highscore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function NumberGuess(x) {
   score = 20;
   HiddenNumber = Math.floor(Math.random() * 15);
 }
-
+let highscore=0;
 NumberGuess();
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -11,6 +11,10 @@ document.querySelector(".check").addEventListener("click", function () {
       //if the guess is correct (It shows that the guess is correct )
       document.querySelector(".checktext").textContent = "Correct Number";
       document.querySelector(".result").textContent = HiddenNumber;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector(".highscore").textContent =`Highscore: ${highscore+1}`;
+      }
     } else if (document.querySelector(".guess").value < HiddenNumber) {
       //if the guess is less than the hidden number
       document.querySelector(".checktext").textContent = "Too Low";
